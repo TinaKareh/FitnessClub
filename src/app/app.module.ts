50ec73f45abc7b10c1f985a6ff1617670ff3b861
Grace Muchuki <gwanjiru35@gmail.com>
2022-02-28 10:14:29 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateMemberComponent } from './create-member/create-member.component';
+import { CreateTrainerComponent } from './create-trainer/create-trainer.component';
+import { CreatePackageComponent } from './create-package/create-package.component';
+import { MembersListComponent } from './members-list/members-list.component';
+import { TrainersListComponent } from './trainers-list/trainers-list.component';
+import { PackageListComponent } from './package-list/package-list.component';
+import { UpdateTrainerComponent } from './update-trainer/update-trainer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateMemberComponent,
+    CreateTrainerComponent,
+    CreatePackageComponent,
+    MembersListComponent,
+    TrainersListComponent,
+    PackageListComponent,
+    UpdateTrainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-trainer/create-trainer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-trainer',
+  templateUrl: './create-trainer.component.html',
+  styleUrls: ['./create-trainer.component.css']
+})
+export class CreateTrainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
